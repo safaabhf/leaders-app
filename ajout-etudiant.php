39abb401d7533762d29aaaedd81f6eee92002263
Safa BELHADJ FRADJ <safabhf03@gmail.com>
2024-12-04 11:42:01 +0100

    added ajout-etudiant.php

---
+++ ajout-etudiant.php
@@ -0,0 +1,20 @@
+
+<html>
+    <head><title> Etudiant</title>
+</head>
+<body>
+    <form action="" method="post">
+    <fieldset>
+        <legend>Etudiant</legend>
+        <label>Nom</label>
+        <input type="text" name="Nom"> <br>
+        <label>Prenom</label>
+        <input type="text" name="prenom"> <br>
+        <label>Email</label>
+        <input type="email"> <br>
+        <input type="submit" value="Ajouter">
+        <input type="reset" value="Annuler">
+    </fieldset>
+    </form>
+</body>
+</html>
